5aa1a12a086fcac62e3da3aa737b76279a949021
Mustafa BATI <mustafa.bati9@gmail.com>
2022-06-07 23:17:50 +0300

    SignIn

--- src/app/ui/components/components.module.ts
+++ src/app/ui/components/components.module.ts
@@ -3,21 +3,20 @@ import { CommonModule } from '@angular/common';
 import { ProductsModule } from './products/products.module';
 import { BasketsModule } from './baskets/baskets.module';
 import { HomeModule } from './home/home.module';
-import { RegisterComponent } from './register/register.component';
 import { RegisterModule } from './register/register.module';
+import { LoginModule } from './login/login.module';
 
 
 
 @NgModule({
-  declarations: [
-    
-  ],
+  declarations: [],
   imports: [
     CommonModule,
     ProductsModule,
     BasketsModule,
     HomeModule,
-    RegisterModule
+    RegisterModule,
+    LoginModule
   ]
 })
 export class ComponentsModule { }
