93f5da377b290f08d08605b62a68ea7e8afca4f8
Mustafa BATI <mustafa.bati9@gmail.com>
2022-06-11 22:41:25 +0300

    JWT

--- src/app/services/common/models/user.service.ts
+++ src/app/services/common/models/user.service.ts
@@ -1,7 +1,10 @@
+import { Token } from '@angular/compiler';
 import { Injectable } from '@angular/core';
 import { firstValueFrom, Observable } from 'rxjs';
+import { TokenResponse } from 'src/app/contracts/token/tokenResponse';
 import { Create_User } from 'src/app/contracts/user/create_user';
 import { User } from 'src/app/entities/user';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from '../../ui/custom-toastr.service';
 import { HttpClientService } from '../http-client.service';
 
 @Injectable({
@@ -9,7 +12,7 @@ import { HttpClientService } from '../http-client.service';
 })
 export class UserService {
 
-  constructor(private httpClientService : HttpClientService) { }
+  constructor(private httpClientService : HttpClientService, private toastrService: CustomToastrService) { }
 
   async create(user: User): Promise<Create_User> {
     const observable: Observable<Create_User | User> =  this.httpClientService.post<Create_User | User>({
@@ -19,13 +22,22 @@ export class UserService {
     return await firstValueFrom(observable) as Create_User;
   }
 
-  async login(usernameOrEmail: string, password: string, callBackFunction?: () => void): Promise<void>{
-    const observable: Observable<any> =  this.httpClientService.post({
+  async login(usernameOrEmail: string, password: string, callBackFunction?: () => void): Promise<any>{
+    const observable: Observable<any | TokenResponse> =  this.httpClientService.post<any | TokenResponse>({
       controller: "users",
       action: "login"
     },{ usernameOrEmail, password })
 
-    await firstValueFrom(observable);
+    const tokenResponse : TokenResponse = await firstValueFrom(observable) as TokenResponse;
+    if(tokenResponse){
+      localStorage.setItem("accessToken", tokenResponse.token.accessToken);
+
+      this.toastrService.message("Giriş Başarılı","Success", {
+        messageType: ToastrMessageType.Success,
+        position: ToastrPosition.TopRight
+      })
+    }
+      
     callBackFunction();
   }
 }
