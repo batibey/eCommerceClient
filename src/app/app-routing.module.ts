fa4496e10fc28057f0306ae8d3c899bd6e9cb621
Mustafa BATI <mustafa.bati9@gmail.com>
2022-10-08 14:23:42 +0300

    forgot  my password

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ import { HomeComponent } from './ui/components/home/home.component';
 const routes: Routes = [
   {
     path: "admin", component: LayoutComponent, children: [
-      { path: "", component: DashboardComponent },
+      { path: "", component: DashboardComponent, canActivate: [AuthGuard] },
       { path: "customers", loadChildren: () => import("./admin/components/customer/customer.module").then(module => module.CustomerModule), canActivate: [AuthGuard] },
       { path: "products", loadChildren: () => import("./admin/components/products/products.module").then(module => module.ProductsModule), canActivate: [AuthGuard] },
       { path: "orders", loadChildren: () => import("./admin/components/order/order.module").then(module => module.OrderModule), canActivate: [AuthGuard] },
@@ -20,6 +20,8 @@ const routes: Routes = [
   { path: "products/:pageNo", loadChildren: () => import("./ui/components/products/products.module").then(module => module.ProductsModule) },
   { path: "register", loadChildren: () => import("./ui/components/register/register.module").then(module => module.RegisterModule) },
   { path: "login", loadChildren: () => import("./ui/components/login/login.module").then(module => module.LoginModule) },
+  { path: "password-reset", loadChildren: () => import("./ui/components/password-reset/password-reset.module").then(module => module.PasswordResetModule) },
+  { path: "update-password/:userId/:resetToken", loadChildren: () => import("./ui/components/update-password/update-password.module").then(module => module.UpdatePasswordModule) },
 ];
 
 @NgModule({
--- src/app/services/common/models/user.service.ts
+++ src/app/services/common/models/user.service.ts
@@ -11,16 +11,29 @@ import { HttpClientService } from '../http-client.service';
   providedIn: 'root'
 })
 export class UserService {
-
-  constructor(private httpClientService : HttpClientService, private toastrService: CustomToastrService) { }
+  constructor(private httpClientService: HttpClientService, private toastrService: CustomToastrService) { }
 
   async create(user: User): Promise<Create_User> {
-    const observable: Observable<Create_User | User> =  this.httpClientService.post<Create_User | User>({
+    const observable: Observable<Create_User | User> = this.httpClientService.post<Create_User | User>({
       controller: "users"
-    },user);
+    }, user);
 
     return await firstValueFrom(observable) as Create_User;
   }
 
-  
+  async updatePassword(userId: string, resetToken: string, password: string, passwordConfirm: string, successCallBack?: () => void, errorCallBack?: (error) => void) {
+    const observable: Observable<any> = this.httpClientService.post({
+      action: "update-password",
+      controller: "users"
+    }, {
+      userId: userId,
+      resetToken: resetToken,
+      password: password,
+      passwordConfirm: passwordConfirm
+    });
+
+    const promiseData: Promise<any> = firstValueFrom(observable);
+    promiseData.then(value => successCallBack()).catch(error => errorCallBack(error));
+    await promiseData;
+  }
 }
