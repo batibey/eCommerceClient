7b07d01325f79fc9982f4545514d9580cbdc3605
Mustafa BATI <mustafa.bati9@gmail.com>
2022-10-23 07:54:38 +0300

    order completed

---
+++ src/app/dialogs/complete-order-dialog/complete-order-dialog.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CompleteOrderDialogComponent } from './complete-order-dialog.component';
+
+describe('CompleteOrderDialogComponent', () => {
+  let component: CompleteOrderDialogComponent;
+  let fixture: ComponentFixture<CompleteOrderDialogComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CompleteOrderDialogComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CompleteOrderDialogComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
