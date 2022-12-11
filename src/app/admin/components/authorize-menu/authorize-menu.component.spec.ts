c3c8e25c96b9eff5167595807e2ac578242a9242
Mustafa BATI <mustafa.bati9@gmail.com>
2022-12-11 17:06:22 +0300

    authorize-menu-dialog-component totalCount error

---
+++ src/app/admin/components/authorize-menu/authorize-menu.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AuthorizeMenuComponent } from './authorize-menu.component';
+
+describe('AuthorizeMenuComponent', () => {
+  let component: AuthorizeMenuComponent;
+  let fixture: ComponentFixture<AuthorizeMenuComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AuthorizeMenuComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AuthorizeMenuComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/admin/components/components.module.ts
+++ src/app/admin/components/components.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ProductsComponent } from './products/products.component';
 import { ProductsModule } from './products/products.module';
 import { CustomerModule } from './customer/customer.module';
 import { OrderModule } from './order/order.module';
 import { DashboardModule } from './dashboard/dashboard.module';
+import { AuthorizeMenuModule } from './authorize-menu/authorize-menu.module';
+import { RoleModule } from './role/role.module';
 
 
 
@@ -15,7 +16,9 @@ import { DashboardModule } from './dashboard/dashboard.module';
     ProductsModule,
     OrderModule,
     CustomerModule,
-    DashboardModule
+    DashboardModule,
+    AuthorizeMenuModule,
+    RoleModule
   ]
 })
 
