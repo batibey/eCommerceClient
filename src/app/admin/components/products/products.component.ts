a0d79e18ea7b8197a87ec0ef2e02545123f48550
Mustafa BATI <mustafa.bati9@gmail.com>
2022-12-27 22:07:17 +0300

    qrCode-reading-dialog

--- src/app/admin/components/products/products.component.ts
+++ src/app/admin/components/products/products.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
 import { Create_Product } from 'src/app/contracts/create_product';
+import { QrcodeReadingDialogComponent } from 'src/app/dialogs/qrcode-reading-dialog/qrcode-reading-dialog.component';
+import { DialogService } from 'src/app/services/common/dialog.service';
 import { HttpClientService } from 'src/app/services/common/http-client.service';
 import { ListComponent } from './list/list.component';
 
@@ -13,7 +15,7 @@ import { ListComponent } from './list/list.component';
 })
 export class ProductsComponent extends BaseComponent implements OnInit {
 
-  constructor(spinner : NgxSpinnerService, private httpClientService: HttpClientService) {
+  constructor(spinner : NgxSpinnerService, private httpClientService: HttpClientService, private dialogService : DialogService) {
     super(spinner);
    }
 
@@ -27,4 +29,14 @@ export class ProductsComponent extends BaseComponent implements OnInit {
     this.listComponents.getProducts();
   }
 
+  showProductQrCodeReading(){
+    this.dialogService.openDialog({
+      componentType: QrcodeReadingDialogComponent,
+      data: null,
+      options: {
+        width: "1000px"
+      },
+      afterClosed: () => { }
+    });
+  }
 }
